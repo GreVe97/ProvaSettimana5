8c75771a71ce0f38118206f78385e440e1bdd6d2
Gregorio Vecchio <gregoriovecchio@gmail.com>
2023-11-28 00:21:48 +0100

    Seconda Pushata

---
+++ S5-P-Medium-Clone-Start/assets/script.js
@@ -0,0 +1,24 @@
+
+let header = document.querySelector('header');
+let altezza= document.querySelector('#accantoAlleM h3').offsetTop;
+window.addEventListener('scroll', function () {   
+    let scrollY = window.scrollY || window.pageYOffset;    
+    if (scrollY > altezza) {
+        header.classList.add('sticky'); 
+    } else {
+        header.classList.remove('sticky'); 
+    }
+});
+
+function mFantasmini() {
+    let mmm = document.querySelectorAll('g[opacity]');
+    let n = Math.floor(Math.random() * mmm.length-1)+1;
+    if(mmm[n].attributes.opacity.value==1){
+        mmm[n].attributes.opacity.value=0
+    }else{
+        mmm[n].attributes.opacity.value=1
+    }
+}
+mFantasmini();
+setInterval(mFantasmini, 50); 
+
